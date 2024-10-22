4afb3840f7fbeb2db271c89c4235efccd0d0d736
wise <3058524082@qq.com>
2024-10-22 20:02:32 +0800

    封装响应

--- api/settings_api/settings_info.go
+++ api/settings_api/settings_info.go
@@ -1,7 +1,12 @@
 package settings_api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gvb_server/common/res"
+)
 
 func (SettingsApi) SettingsInfoView(c *gin.Context) {
-	c.JSON(200, gin.H{"msg": "xxx"})
+	//c.JSON(200, gin.H{"msg": "xxx"})
+	//res.Ok(map[string]string{"int": "haha"}, "xxx", c)
+	res.FailWithCode(res.SettingsError, c)
 }
