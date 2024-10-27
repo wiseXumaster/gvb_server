26a0f733c708e67127d37cc1b89577aea3021da7
wise <3058524082@qq.com>
2024-10-27 14:13:58 +0800

    忽略setting.yaml

--- main.go
+++ main.go
@@ -12,11 +12,6 @@ func main() {
 	core.InitConf()
 	// 初始化日志
 	global.Log = core.InitLogger()
-	//global.Log.Println("ok")
-	//global.Log.Warnln("warn")
-	//global.Log.Error("warn")
-	//global.Log.Infof("warn")
-	//logrus.Warn("hh")
 	// 连接数据库
 	global.DB = core.InitGorm()
 
@@ -25,8 +20,6 @@ func main() {
 		cmd.SwitchOption(option)
 		return
 	}
-	//fmt.Println(global.DB)
-	//fmt.Println(global.Config)
 	router := routers.InitRouter()
 	addr := global.Config.System.Addr()
 	global.Log.Infof("gvb_server运行在: %s", addr)
